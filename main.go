9f1c8cba7f95029ac67173a8bface89f6d4e08cd
Olaf Flebbe <of@oflebbe.de>
2020-09-06 12:01:58 +0200

    not ready

--- main.go
+++ main.go
@@ -16,19 +16,8 @@ import (
 	"sync"
 )
 
-// read in gaia file
-
-// Coord Of the stars
-type Coord struct {
-	Ra  float32
-	Dec float32
-	X   float32
-	Y   float32
-	Z   float32
-}
-
 // ReadFiles read filenames from chanel ch and puts them on chanel output
-func ReadFiles(ch *chan string, output *chan []Coord, wg *sync.WaitGroup) {
+func ReadFiles(ch *chan string, cols []int, output *chan []Coord, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for s := range *ch {
 		ReadOneFile(s, output)
@@ -38,7 +27,7 @@ func ReadFiles(ch *chan string, output *chan []Coord, wg *sync.WaitGroup) {
 }
 
 // ReadOneFile reads on file
-func ReadOneFile(fn string, ch *chan []Coord) {
+func ReadOneFile(fn string, cols []int, ch *chan []float32) {
 	file, err := os.Open(fn)
 	if err != nil {
 		log.Fatalf("Could not open %s", fn)
@@ -49,40 +38,38 @@ func ReadOneFile(fn string, ch *chan []Coord) {
 	}
 	scanner := bufio.NewScanner(uncompressor)
 	scanner.Scan() // skip first line
-	var result []Coord
+
+	var result []float32
+
 	for scanner.Scan() {
+		buffer := make([]float32, len(cols), len(cols))
 		line := scanner.Text()
 		toks := strings.Split(line, ",")
-		ra, err := strconv.ParseFloat(toks[5], 32)
-		if err != nil {
-			log.Fatalf("line %s not parsable: ra", line)
+		for i, v := range cols {
+			if toks[v] == "" {
+				buffer[i] = float32(math.NaN())
+			}
+			val, err := strconv.ParseFloat(toks[v], 32)
+			if err != nil {
+				log.Fatalf("line %s not parsable: ra", line)
+			}
+			buffer[i] = float32(val)
 		}
 
-		dec, err := strconv.ParseFloat(toks[7], 32)
-		if err != nil {
-			log.Fatalf("line %s not parsable: dec", line)
-		}
-
-		if toks[9] == "" {
-			continue
-		}
-		parallax, err := strconv.ParseFloat(toks[9], 32)
-		if err != nil {
-			log.Fatalf("line %s not parsable parallax %s", line, toks[9])
-		}
-		if parallax < 0. {
-			// https://astronomy.stackexchange.com/questions/26250/what-is-the-proper-interpretation-of-a-negative-parallax
-			continue
-		}
-		sra, cra := math.Sincos(ra * math.Pi / 180.0)
-		sdec, cdec := math.Sincos(dec * math.Pi / 180.0)
-		r := 1.58125074e-5 / (parallax / (1000 * 3600) * math.Pi / 180.)
-		x := r * cra * cdec
-		y := r * sra * cdec
-		z := r * sdec
-		c := Coord{Ra: float32(ra), Dec: float32(dec), X: float32(x), Y: float32(y), Z: float32(z)}
-		// println(x, y, z)
-
+		/*
+			if parallax < 0. {
+				// https://astronomy.stackexchange.com/questions/26250/what-is-the-proper-interpretation-of-a-negative-parallax
+				continue
+			}
+			sra, cra := math.Sincos(ra * math.Pi / 180.0)
+			sdec, cdec := math.Sincos(dec * math.Pi / 180.0)
+			r := 1.58125074e-5 / (parallax / (1000 * 3600) * math.Pi / 180.)
+			x := r * cra * cdec
+			y := r * sra * cdec
+			z := r * sdec
+			c := Coord{Ra: float32(ra), Dec: float32(dec), X: float32(x), Y: float32(y), Z: float32(z)}
+			// println(x, y, z)
+		*/
 		result = append(result, c)
 	}
 	*ch <- result
@@ -90,6 +77,16 @@ func ReadOneFile(fn string, ch *chan []Coord) {
 }
 
 func main() {
+	numArgs := len(os.Args)
+	var cols []int
+	for i := 1; i < numArgs; i++ {
+		icol, err := strconv.Atoi(os.Args[i])
+		if err != nil {
+			log.Fatal("columns not int")
+		}
+		cols = append(cols, icol)
+	}
+
 	fileInfo, err := ioutil.ReadDir(os.Args[1])
 	if err != nil {
 		log.Fatalf("Could not read %s", os.Args[1])
@@ -126,7 +123,7 @@ func main() {
 	for i := 0; i < runtime.NumCPU(); i++ {
 		go func(wg *sync.WaitGroup) {
 			wg.Add(1)
-			ReadFiles(&fileNameCh, &ch, wg)
+			ReadFiles(&fileNameCh, cols, &ch, wg)
 		}(&wg)
 	}
 
